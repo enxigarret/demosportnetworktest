557e0c4caa105e82fa24cebf74caab2296ccca26
enxigarret <mclawiabroad1@hotmail.fr>
2025-03-02 17:03:10 +0100

    add video et photo alt text.

--- components/video_iframe.tsx
+++ components/video_iframe.tsx
@@ -13,7 +13,8 @@ const FacebookVideoEmbed = () => {
         height="576"
         style={{ border: 'none', overflow: 'hidden' }}
         className='responsive-iframe'
-   
+        title="Vimeo Video: Hadisport Hop 2024" 
+        aria-label="A video presetation of hadisport 2024"
         allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
         allowFullScreen={true}
       ></iframe>
